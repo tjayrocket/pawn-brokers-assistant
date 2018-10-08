9503772886ceef3436f8e141995b14422634895d
TJay Rocket <tjayrocket@gmail.com>
2018-10-07 22:48:31 -0700

    Fixing app breaking bugs.

--- src/components/Loan.jsx
+++ src/components/Loan.jsx
@@ -19,30 +19,74 @@ class Loan extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
 
-    //Just For verification purposes. Will recycle this for number validation later.
-    alert('Your loan amount is: ' + this.state.value);
+    //This sets the base amount to a localized variable. Handy for later. Assigned by the field.
 
-    //This sets the base amount to a localized variable. Handy for later.
     let amount = this.state.value;
 
+    //This function validates the input of numbers into the form.
+
+    function loanValidate(){
+      if(amount === '' || isNaN(amount)){
+        alert('Please enter a loan amount - numbers only, please.');
+        return false;
+      } else {
+        alert('Your loan amount is: ' + amount);
+        return true;
+      }
+    };
+
+      loanValidate();
+
+    let storage = 5;
+    let firearm = 5;
+    let lateFee = 10;
+    let missingTicket = 5;
+
+    let processingInterest = function(amount){
+
+    }
+
+    // let interest;
+    
+    // function interestRate(amount){
+
+    // }
+
+
+    let first;
+    let second;
+    let third;
+    let late;
+    let final;
+
+    let forfeit;
+
+
+    // console.log('Loan amount = $' + parseInt(amount).toFixed(2)); //Loan Amount
+    // console.log('10% = $' + parseInt(amount/10).toFixed(2)); //Interest
+    
+
     //Grabs the first circle area.
-    let blockOne = document.getElementById('loanOneText');
+    // let blockOne = document.getElementById('loanOneRate');
 
     // Places the amount into the circle.
-    blockOne.innerHTML = '$' + parseInt(amount).toFixed(2);
+    // blockOne.innerHTML = '$' + parseInt(amount).toFixed(2);
   }
 
   render() {
     return (
       <div id="loan">
-        <form onSubmit={this.handleSubmit}>
+        <form id="loanForm" onSubmit={this.handleSubmit}>
           <label>
             <p className="subText">Please Enter Your Loan Amount:</p>
             <input type="text" value={this.state.value} onChange={this.handleChange} />
           </label>
+          <input type="checkbox" />
           <input type="submit" value="Submit" />
         </form>
-        <div id="loanAmount"></div>
+        <div id="loanAmount">
+          <p id="loanOneRate" className="loanText">$0.00</p>
+        </div>
         <div id="loanRatesBlock">
           <div id="interestRate" className="rightBlock"></div>
           <div id="processingFee" className="rightBlock"></div>
