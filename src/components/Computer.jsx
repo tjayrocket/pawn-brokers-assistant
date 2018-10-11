9913432bdbd75b9d7ee5814d17e09f793d1b51f3
TJay Rocket <tjayrocket@gmail.com>
2018-10-10 21:53:30 -0700

    Pushing to activate github page for it.

---
+++ src/components/Computer.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Computer extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="Computer">
+        <p>This is the Computer Page</p>
+      </div>
+    );
+  }
+
+}
+
+export default Computer;
---
+++ src/components/FAQ.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class FAQ extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div id="FAQ">
+        <p>This is the FAQ Page</p>
+      </div>
+    );
+  }
+
+}
+
+export default FAQ;
