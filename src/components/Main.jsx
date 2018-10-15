19db61d783ef803ae3a9c85f26d8848a8b063af3
TJay Rocket <tjayrocket@gmail.com>
2018-10-14 22:47:00 -0700

    Working on some layaway ideas

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
 import Loan from './Loan.jsx';
 import Layaway from './Layaway.jsx';
 import Computer from './Computer.jsx';
